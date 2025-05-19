d591ed7386c7a31bccf6a5853bcd2adfff0915ff
Pavel-on-GH <Pavel.Zaitsev-7@yandex.ru>
2025-05-19 04:08:22 +0300

    2.0.0.0 - App выделен в отдельный компонент, в useOutsideClickClose добавлена проверка открытия сайдбара, удалён лишний clsx

--- src/ui/select/hooks/useOutsideClickClose.ts
+++ src/ui/select/hooks/useOutsideClickClose.ts
@@ -14,6 +14,10 @@ export const useOutsideClickClose = ({
 	onChange,
 }: UseOutsideClickClose) => {
 	useEffect(() => {
+		// Проверка, чтобы не вызывать лишних действий при закрытом сайдбаре
+		if (!isOpen) {
+			return;
+		}
 		const handleClick = (event: MouseEvent) => {
 			const { target } = event;
 			if (target instanceof Node && !rootRef.current?.contains(target)) {
